2572b3e9948cd7217d05df39c11bdceddfc79e16
Alexis Lucas <alexis.lucas1@viacesi.fr>
2017-04-19 13:34:57 +0200

    Déclaration des méthodes du contrôleur mère

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -8,22 +8,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 abstract class DefaultController extends Controller
 {
 
-    protected function dbSave($method, $obj_record)
+    protected function dbUpdate($method, $obj_record)
     {
     	$em = $this->getDoctrine()->getManager();
-    	if($method == "persist") {
-    		$em->persist($obj_record);
-    	} else if ($method == "merge") {
-    		$em->merge($obj_record);
-    	} else {
-    		return "Error : unknown method";
+    	switch($method) {
+    		case 'persist':
+    			$em->persist($obj_record);
+    			break;
+    		case 'merge';
+    			$em->merge($obj_record);
+    			break;
+    		case 'remove':
+    			$em->remove($obj_record);
+    			break;
+    		default:
+    			return false;
     	}
+    	$em->flush();
     }
 
-    protected function checkRole($wanted, $cookie) {
-    	//if cookie role == wanted role
-    	//return true
-    	//else
-    	//return false
+    protected function checkRole($wanted, $curent) {
+    	if($current == $wanted) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 }
