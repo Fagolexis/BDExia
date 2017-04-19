0d7f3d3210ec999ad86c45a8d8c901a2c539e2a9
Alexis Lucas <alexis.lucas1@viacesi.fr>
2017-04-19 09:54:06 +0200

    Ajout du contrôleur mère DefaultController

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -5,14 +5,25 @@ namespace MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+abstract class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
+	$em = $this->getDoctrine()->getManager();
+
+    protected function dbSave($method, $obj_record)
     {
-        return $this->render('MainBundle:Accueil:accueil.html.twig', array(
-        ));
+    	if($method == "persist") {
+    		$this->em->persist($obj_record);
+    	} else if ($method == "merge") {
+    		$this->em->merge($obj_record);
+    	} else {
+    		return "Error : unknown method";
+    	}
+    }
+
+    protected function checkRole($wanted, $cookie) {
+    	//if cookie role == wanted role
+    	//return true
+    	//else
+    	//return false
     }
 }
