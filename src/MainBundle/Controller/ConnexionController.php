694ab0826ea3795ce7cb40e517b427b065a9fbcb
François CAILLAT <Franois CAILLAT>
2017-04-20 10:12:06 +0200

    Modification des sessions

--- src/MainBundle/Controller/ConnexionController.php
+++ src/MainBundle/Controller/ConnexionController.php
@@ -14,49 +14,78 @@ class ConnexionController extends DefaultController
      */
     public function ConnexionAction(Request $request)
     {
-       	$mailUser = $request->request->get('mail');
-    	$pswrdUser = $request->request->get('password');
-    	session_name('utilisateur');
-    	$_SESSION['mail'] = $mailUser;
-    	$_SESSION['password'] = $pswrdUser;
-    	$user = $this->getDoctrine()
-        ->getRepository('MainBundle:Users')
-        ->findOneBy(array('mail' => $_SESSION['mail']));
-        $errorMessage ='';
-    	if ($_SESSION['password']=='' && $_SESSION['mail']=='') {
-    		return $this->render('MainBundle:Connexion:connexion.html.twig', array(
-        	'mail'=>$_SESSION['mail'],
-        	'password'=>$_SESSION['password'],
-        	'errormessage' => $errorMessage
-        	));       	
+		$errorMessage = '';
+    	if (isset($_POST['submitConnexion'])) {
+		       	$mailUser = $request->request->get('mail');
+		    	$pswrdUser = $request->request->get('password');
+
+
+		    	$user = $this->getDoctrine()
+		        ->getRepository('MainBundle:Users')
+		        ->findOneBy(array('mail' => $mailUser));
+
+		        $errorMessage ='';
+
+		    	if ($pswrdUser!='' && $mailUser!='') {	    		
+		        	if ($user!='') {
+		        		if ($user->getMdp()==$pswrdUser) {
+
+					    	session_name('$user->getIdUser()');
+					    	$_SESSION['idUser'] = $user->getIdUser();
+					    	$_SESSION['roleUser'] = $user->getRoleUser();
+				    		session_start();
+						    return $this->render('MainBundle:Accueil:accueil.html.twig', array(
+				        	'user'=>$user
+				        	)); 
+		        		}   
+		        		else {
+			    			$errorMessage = 'mauvais mot de passe';
+				    		return $this->render('MainBundle:Connexion:connexion.html.twig', array(
+			        		'errormessage' => $errorMessage
+				        	)); 
+		        		}	
+		       		}
+			        else {
+		    			$errorMessage = 'cet utilisateur n\'existe pas';
+			    		return $this->render('MainBundle:Connexion:connexion.html.twig', array(
+		        		'errormessage' => $errorMessage
+			        	)); 
+			        }
+		    	}
     	}
-    	elseif ($_SESSION['password']!='' && $_SESSION['mail']!='') {	    		
-        	if ($user!='') {
-        		if ($user->getMdp()==$pswrdUser = $request->get('password')) {
-		    		session_start();
-				    return $this->render('MainBundle:Accueil:accueil.html.twig', array(
-		        	'mail'=>$_SESSION['mail'],
-		        	'password'=>$_SESSION['password']
-		        	)); 
-        		}   
-        		else{
-	    			$errorMessage = 'mauvais mot de passe';
-		    		return $this->render('MainBundle:Connexion:connexion.html.twig', array(
-		        	'mail'=>$_SESSION['mail']
-,		        	'password'=>$_SESSION['password'],
-	        		'errormessage' => $errorMessage
-		        	)); 
-        		}	
-       		}   
-	        else {
-    			$errorMessage = 'cet utilisateur n\'existe pas';
-	    		return $this->render('MainBundle:Connexion:connexion.html.twig', array(
-	        	'mail'=>$_SESSION['mail'],
-	        	'password'=>$_SESSION['password'],
-        		'errormessage' => $errorMessage
-	        	)); 
-	        }
-    	} 
-    	session_write_close();
+    	
+    	elseif (isset($_POST['submitInscription'])) {
+		       	$NomlUser = $request->request->get('Nom');
+		    	$PrenomUser = $request->request->get('Prenom');
+		       	$EmailUser = $request->request->get('Email');
+		    	$PswrdUser = $request->request->get('Password');
+		    	$PromoUser = $request->request->get('Promotion');
+		       	$AvatarUser = $request->request->get('Avatar');
+		       	$PhoneUser = $request->request->get('Telephone');
+
+		    	$user = $this->getDoctrine()
+		        ->getRepository('MainBundle:Users')
+		        ->findOneBy(array('mail' => $EmailUser));
+
+		    	if ($NomlUser!='' && $PrenomUser!='' && $EmailUser!='' && $PswrdUser!='') {
+
+					session_name('$user->getIdUser()');
+					$_SESSION['idUser'] = $user->getIdUser();
+					$_SESSION['roleUser'] = $user->getRoleUser();
+
+		    		return $this->render('MainBundle:Gestion:profil.html.twig', array(
+		        	'user'=>$user
+		        	));     		
+		    	}
+		    	else {
+		    		$errorMessage = 'non valide';
+			    	return $this->render('MainBundle:Connexion:connexion.html.twig', array(
+		        	'errormessage' => $errorMessage
+			       	)); 
+		    	}
+    		}	
+		return $this->render('MainBundle:Connexion:connexion.html.twig', array(
+		'errormessage' => $errorMessage
+		)); 
     }
 }
