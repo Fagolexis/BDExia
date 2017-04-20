cabb38b7f8585fe697223b8d42dc6aff1ac4330a
Alexis Lucas <alexis.lucas1@viacesi.fr>
2017-04-21 00:34:35 +0200

    Commit defaultcontroller

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -4,14 +4,71 @@ namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+abstract class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
+
+    protected function dbUpdate($method, $obj_record)
     {
-        return $this->render('MainBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+    	switch($method) {
+    		case 'persist':
+    			$em->persist($obj_record);
+    			break;
+    		case 'merge';
+    			$em->merge($obj_record);
+    			break;
+    		case 'remove':
+    			$em->remove($obj_record);
+    			break;
+    		default:
+    			return false;
+    	}
+    	$em->flush();
+    }
+
+    protected function checkRole($wanted, $current) {
+    	if($current == $wanted) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+
+    protected function saveImg($img) {
+        $dossier = '/web/img/';
+        $fichier = basename($img->getClientOriginalName());
+        $taille_maxi = 10000000;
+        $taille = $img->getClientSize();
+        $extensions = array('image/png', 'image/gif', 'image/jpg', 'image/jpeg');
+        $extension = $img->getMimeType();
+        if(!in_array($extension, $extensions)) {
+            $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg.';
+        }
+        if($taille>$taille_maxi) {
+            $erreur = 'Le fichier est trop volumineux.';
+        }
+        if(!isset($erreur)) {
+            $fichier = strtr($fichier, 
+                'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+                'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+            $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+
+            $img->move($dossier, $fichier);
+            if(file_exists($dossier.$fichier)) {
+                $erreur = 'Echec de l\'upload !';
+            }
+
+            return $fichier;
+        }
+        else {
+            return $erreur;
+        }
+    }
+
+    protected function insert($array) {
+        $this->getDoctrine()->getConnection()->executeUpdate($array['sql'], $array['param']);
     }
 }
